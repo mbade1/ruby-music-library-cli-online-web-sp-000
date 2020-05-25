95a556513b6c82d1955b86199e0fc93e56683a1c
Michael Bade <mbade314@gmail.com>
2020-05-25 22:21:14 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,46 @@
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.create(genre)
+    genre = self.new(genre)
+    genre.save
+    genre
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
+  end
+
+end
