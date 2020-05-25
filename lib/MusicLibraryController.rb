c0a531c231649ff2772b68634eb9bbb501450bc1
Michael Bade <mbade314@gmail.com>
2020-05-25 22:19:28 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -7,8 +7,9 @@ class MusicLibraryController
   end
 
   def call
-    # input = gets.strip
-    # if input != exit
+    input = gets.strip
+    if input != exit
+      
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -18,27 +19,25 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-    # end
-
-
-
-
+      
+      case input
+        when "list songs"
+          list_songs_
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play_song"
+          play_song
+        end
+      end
     end
 
-    # case input
-    #   when "list songs"
-    #     list_songs_
-    #   when "list artists"
-    #     list_artists
-    #   when "list genres"
-    #     list_genres
-    #   when "list artist"
-    #     list_songs_by_artist
-    #   when "list genre"
-    #     list_songs_by_genre
-    #   when "play_song"
-    #     play_song
-    #   end
+
 
 
 
