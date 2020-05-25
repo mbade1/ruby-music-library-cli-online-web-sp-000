436c1c8bc6c8aa08a1f3322bde59c519f3a78e20
Michael Bade <mbade314@gmail.com>
2020-05-25 21:44:45 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -9,4 +9,10 @@ class MusicImporter
   def files
     Dir.entries(@path).select {|file| file.end_with?(".mp3")}
   end
+
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end
 end
