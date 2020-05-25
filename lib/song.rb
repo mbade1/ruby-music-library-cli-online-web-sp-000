9da16bdfaed80275c344d5b7e82b19124350b5ed
Michael Bade <mbade314@gmail.com>
2020-05-25 21:06:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
 
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     @artist = artist
     self.artist=(artist) if artist != nil
