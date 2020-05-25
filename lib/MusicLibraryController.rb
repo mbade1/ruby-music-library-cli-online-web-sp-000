18aba6ac5263412b4d878eb3ce19bb35cb466cff
Michael Bade <mbade314@gmail.com>
2020-05-25 22:07:09 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -46,6 +46,7 @@ class MusicLibraryController
   def list_genres
     Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |g, i|
       puts "#{i + 1}. #{g.name}"
+    end
   end
 
   def list_songs_by_artist
