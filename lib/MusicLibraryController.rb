a4e9eb6c4e9ca767778785bc5167d54c9113b28c
Michael Bade <mbade314@gmail.com>
2020-05-25 21:58:33 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -7,9 +7,56 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "What would you like to do?"
+    input = gets.strip
+    if input != exit
+      puts "Welcome to your music library!"
+      puts "What would you like to do?"
 
+    case input
+      when "list songs"
+        list_songs_
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play_song"
+        play_song
+      end
+  end
+
+
+  def list_songs
 
   end
+
+  def list_artists
+
+  end
+
+  def list_genres
+
+  end
+
+  def list_songs_by_artist
+
+  end
+
+  def list_songs_by_genre
+
+  end
+
+  def play_song
+
+  end
+
+  
+
+
+
+
+  
 end
