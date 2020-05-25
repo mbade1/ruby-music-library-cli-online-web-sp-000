30100351b887b49dc3cb3373f8017694f73f772b
Michael Bade <mbade314@gmail.com>
2020-05-25 21:48:05 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
 
   def initilaize(path = './db/mp3s/')
-    music_importer_obj = self.new(path)
+    music_importer_obj = self.new
     music_importer_obj.import
   end
 
