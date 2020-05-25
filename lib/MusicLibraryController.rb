102afe0ca1cb4c5685b44983c854ba41601d1bab
Michael Bade <mbade314@gmail.com>
2020-05-25 22:05:45 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -39,7 +39,7 @@ class MusicLibraryController
 
   def list_artists
     Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
-      puts "#{i + 1}. #{a.artist.name}"
+      puts "#{i + 1}. #{a.name}"
     end
   end
 
