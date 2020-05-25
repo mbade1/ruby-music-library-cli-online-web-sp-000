e813a960d105e0c69326aee6c7635df8ea693bc7
Michael Bade <mbade314@gmail.com>
2020-05-25 21:23:19 +0000

    Automatically backed up by Learn

--- lib/concerns/finadble.rb
+++ lib/concerns/finadble.rb
@@ -1,3 +1,12 @@
 module Concerns::Findable
+  def self.find_by_name(name)
+    self.all.detect do |x|
+      x.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
 
 end
