63913973e1e77f0ba1a0a2f6884f47a677b8a191
Michael Bade <mbade314@gmail.com>
2020-05-25 22:13:59 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -7,7 +7,7 @@ class MusicLibraryController
   end
 
   def call
-    
+
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "What would you like to do?"
