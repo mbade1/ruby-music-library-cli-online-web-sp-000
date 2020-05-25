2daa82fdc2c655c85dfb4bfe7a3bfe91015bc745
Michael Bade <mbade314@gmail.com>
2020-05-25 21:14:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,5 +43,13 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
+  def self.find_by_name
+    @@all.detect do |song|
+      song.name = name
+    end
+  end
+      
+    
+
 
 end
