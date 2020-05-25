a299335b6d463c0621782a94a108447f07628bff
Michael Bade <mbade314@gmail.com>
2020-05-25 21:02:18 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,54 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+
+    if @songs.include?(song)
+      nil
+    else
+      @songs << song
+    end
+    song
+  end
+    #if the current artist is already listed,
+      #don't add it.
+    #otherwise, do add it
+    #if the song is already
+
+  def songs
+    @songs
+  end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+
+end
